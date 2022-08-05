119ce2c49e8e0689f8b1437501576bc5d1159f20
surekha3 <surekhakarna3@gmail.com>
2022-08-05 15:09:45 +0530

    modified

--- dev/main.tf
+++ dev/main.tf
@@ -1,6 +1,6 @@
 module "TF_MODULE_VPC" {
   source		= "../modules/vpc/"
-  cidr			= var.vpc_cidr
-  vpc_tag_name		= var.vpc_tag_name
-  vpc_tag_environment	= var.vpc_tag_environment
+  vpc_cidr			= var.vpc_cidr
+  vpc_name		= var.vpc_tag_name
+  tag_environment	= var.vpc_tag_environment
 }
